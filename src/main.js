72c9b4841d713cc9b086660b24e3d80fc92c6de0
rulo-dev <rulosan77@gmail.com>
2025-02-24 23:23:47 -0600

    agregando tablero

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import './style.css';
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const BLOCK_SIZE = 30;
-const BOARD_WIDTH = 10;
-const BOARD_HEIGHT = 20;
+const BLOCK_SIZE = 20;
+const BOARD_WIDTH = 11;
+const BOARD_HEIGHT = 30;
 
 //2.- Crear el tablero
 canvas.width = BLOCK_SIZE * BOARD_WIDTH;
@@ -14,16 +14,63 @@ canvas.height = BLOCK_SIZE * BOARD_HEIGHT;
 
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
 
+//4.- Dibujar el tablero
+const board = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 1, 0, 0, 1, 1],
+];
+
 //3.- Game Loop
 function update() {
   draw();
-  requestAnimationFrame(update);
+  window.requestAnimationFrame(update);
 }
 
 function draw() {
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  drawBoard();
+
+  board.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value === 1) {
+        ctx.fillStyle = 'yellow';
+        ctx.fillRect(x, y, 1, 1);
+      }
+    });
+  });
 }
 
 update();
