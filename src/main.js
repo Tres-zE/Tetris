de15f369346301a8ef25f52dec2662f73160c7b5
rulo-dev <rulosan77@gmail.com>
2025-02-26 22:54:19 -0600

    agregando colision hacia abajo y solidificando piezas

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 const BLOCK_SIZE = 20;
-const BOARD_WIDTH = 10;
+const BOARD_WIDTH = 14;
 const BOARD_HEIGHT = 30;
 
 //2.- Crear el tablero
@@ -95,10 +95,16 @@ function draw() {
 document.addEventListener('keydown', (event) => {
   if (event.key === 'ArrowDown') {
     piece.position.y++;
+    if (checkCollision()) {
+      piece.position.y--;
+    }
   }
 
   if (event.key === 'ArrowRight') {
     piece.position.x++;
+    if (checkCollision()) {
+      piece.position.x--;
+    }
   }
 
   if (event.key === 'ArrowLeft') {
@@ -114,12 +120,23 @@ function checkCollision() {
   return piece.shape.find((row, y) => {
     return row.find((value, x) => {
       return (
-        value != 0 &&
-        board[y + piece.position.y] &&
-        board[y + piece.position.y][x + piece.position.x] != 0
+        value != 0 && board[y + piece.position.y]?.[x + piece.position.x] != 0
       );
     });
   });
+}
+
+function solidifyPiece() {
+  piece.shape.forEach((row, x) => {
+    row.forEach((value, y) => {
+      if (value === 1) {
+        board[y + piece.position.y][x + piece.position.x] = 1;
+      }
+    });
+  });
+
+  piece.position.x = 0;
+  piece.position.y = 0;
 }
 
 update();
